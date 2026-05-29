b68c1dfc905e57c77138885e88e0db4862e00ff7
Raphael Wilson Silva <112573817+RWSilva1990@users.noreply.github.com>
2026-05-29 12:54:53 -0300

    Change background color from dark to white

--- src/App.jsx
+++ src/App.jsx
@@ -53,7 +53,7 @@ const GLOBAL_CSS = `
  *, *::before, *::after { box-sizing: border-box; margin: 0; padding: 0; }
  html, body, #root { width: 100%; max-width: 100vw; overflow-x: hidden; }
  html, body { 
-   background: #070C14; 
+   background: #FFFFFF; 
    font-family: 'Nunito', sans-serif; 
    color: #EDF2F8; 
    -webkit-tap-highlight-color: transparent; 
