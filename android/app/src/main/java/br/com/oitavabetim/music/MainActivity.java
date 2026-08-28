fc8c122603ce51931097928e5d694907afdeaca9
Raphael Wilson Silva <112573817+RWSilva1990@users.noreply.github.com>
2026-08-28 11:35:14 -0300

    refactor: restore default Capacitor navigation for packaged app

--- android/app/src/main/java/br/com/oitavabetim/music/MainActivity.java
+++ android/app/src/main/java/br/com/oitavabetim/music/MainActivity.java
@@ -1,28 +1,5 @@
 package br.com.oitavabetim.music;
 
-import android.os.Bundle;
-import android.webkit.WebResourceRequest;
-import android.webkit.WebView;
-
 import com.getcapacitor.BridgeActivity;
-import com.getcapacitor.BridgeWebViewClient;
 
-public class MainActivity extends BridgeActivity {
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-
-        if (bridge != null && bridge.getWebView() != null) {
-            bridge.getWebView().setWebViewClient(new BridgeWebViewClient(bridge) {
-                @Override
-                public boolean shouldOverrideUrlLoading(WebView view, WebResourceRequest request) {
-                    String host = request.getUrl().getHost();
-                    if (host != null && host.toLowerCase().endsWith(".vercel.app")) {
-                        return false;
-                    }
-                    return super.shouldOverrideUrlLoading(view, request);
-                }
-            });
-        }
-    }
-}
+public class MainActivity extends BridgeActivity {}
