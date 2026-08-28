24bb4a57d39052312bf53133a5f6abbf883cb83c
Raphael Wilson Silva <112573817+RWSilva1990@users.noreply.github.com>
2026-08-28 11:58:13 -0300

    fix: navigate app history on Android system back

--- android/app/src/main/java/br/com/oitavabetim/music/MainActivity.java
+++ android/app/src/main/java/br/com/oitavabetim/music/MainActivity.java
@@ -1,5 +1,47 @@
 package br.com.oitavabetim.music;
 
+import android.os.Bundle;
+import android.webkit.WebView;
+
+import androidx.activity.OnBackPressedCallback;
+
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        getOnBackPressedDispatcher().addCallback(this, new OnBackPressedCallback(true) {
+            @Override
+            public void handleOnBackPressed() {
+                WebView webView = bridge != null ? bridge.getWebView() : null;
+                if (webView == null) {
+                    exitThroughSystemBack();
+                    return;
+                }
+
+                webView.evaluateJavascript(
+                    "(function(){" +
+                        "if(window.history.length>1){" +
+                            "window.history.back();" +
+                            "return true;" +
+                        "}" +
+                        "return false;" +
+                    "})()",
+                    result -> {
+                        if (!"true".equals(result)) {
+                            exitThroughSystemBack();
+                        }
+                    }
+                );
+            }
+
+            private void exitThroughSystemBack() {
+                setEnabled(false);
+                getOnBackPressedDispatcher().onBackPressed();
+                setEnabled(true);
+            }
+        });
+    }
+}
